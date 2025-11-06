dbc5d75f125daea5894e5a2aaced26515aa54b82
Mike Dyer <mike.dyer@riverside.fm>
2025-11-06 16:33:21 +1100

    Fix: Preserve std::optional wrapper for substituted types in optional fields

--- test/languages.ts
+++ test/languages.ts
@@ -536,7 +536,7 @@ export const CPlusPlusLanguage: Language = {
     base: "test/fixtures/cplusplus",
     setupCommand:
         "curl -o json.hpp https://raw.githubusercontent.com/nlohmann/json/87df1d6708915ffbfa26a051ad7562ecc22e5579/src/json.hpp",
-    compileCommand: "g++ -O0 -o quicktype -std=c++17 main.cpp",
+    compileCommand: "g++ -O0 -o quicktype -std=c++17 -I/opt/homebrew/opt/boost/include main.cpp",
     runCommand(sample: string) {
         return `./quicktype "${sample}"`;
     },
